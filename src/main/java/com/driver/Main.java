0984463e8c4a5485b4fa9ec03b21dcb1b71f20da
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-03-24 04:58:07 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // obj.name = "Rohith";
+        //  System.out.print(obj.name);
+        //name has private access in com.driver.RWOnly
+        String name = obj.get();
+        obj.set("Rohith");
+        System.out.print(obj.get());
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public String get(){
+        return name;
+    }
+    public void set(String newName){
+        this.name = newName;
+    }
 }
